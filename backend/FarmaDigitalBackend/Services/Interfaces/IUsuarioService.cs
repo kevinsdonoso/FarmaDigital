71c3fe6f9bd2c426e2a52862e7ef9b4b1a4fc623
Joselyn Pilco <pasante.it@sime.com.ec>
2025-07-20 16:04:43 -0500

    changes

--- backend/FarmaDigitalBackend/Services/Interfaces/IUsuarioService.cs
+++ backend/FarmaDigitalBackend/Services/Interfaces/IUsuarioService.cs
@@ -5,5 +5,6 @@ namespace FarmaDigitalBackend.Services.Interfaces
     public interface IUsuarioService
     {
         Task<Usuario?> GetUserByDni(string dni);
+        Task<Usuario?> GetUserFromToken(System.Security.Claims.ClaimsPrincipal userClaims);
     }
 }
